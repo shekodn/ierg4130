4fc780974241c16049f67edec55471aae5112dd9
shekodn <sdiazn19@gmail.com>
2017-10-20 15:50:05 +0800

    updates java code

--- lab2/xss/HTTPSimpleForge.java
+++ lab2/xss/HTTPSimpleForge.java
@@ -6,7 +6,7 @@ public class HTTPSimpleForge {
       int responseCode;
       InputStream responseIn=null;
       // URL to be forged.
-      URL url = new URL ("http://www.xsslabphpbb.com/profile.php");
+      URL url = new URL ("http://www.xsslabphpbb.com/posting.php");
       // URLConnection instance is created to further parameterize a
       // resource request past what the state members of URL instance
       // can represent.
@@ -40,6 +40,9 @@ public class HTTPSimpleForge {
         // HTTP status code HTTP_OK means the response was
         // received sucessfully.
         if (responseCode == HttpURLConnection.HTTP_OK) {
+
+          System.out.println("responseCode is 200 :D");
+
           // Get the input stream from url connection object.
           responseIn = urlConn.getInputStream();
           // Create an instance for BufferedReader
@@ -49,10 +52,15 @@ public class HTTPSimpleForge {
           String inputLine;
           while((inputLine = buf_inp.readLine())!=null) {
             System.out.println(inputLine);
-          } }
+          }
+
+
+        } else {
+          System.out.println("responseCode is NOT 200");
         }
-      } catch (MalformedURLException e) {
-        e.printStackTrace();
       }
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
     }
   }
+}
